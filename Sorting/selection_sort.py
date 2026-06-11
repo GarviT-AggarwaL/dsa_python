1c18e60c35853dbfa985278e9020b9a274ff850c
unknown <aggarwalgarvit1304@gmail.com>
2026-06-11 13:42:30 +0530

    learned selection sort

---
+++ Sorting/selection_sort.py
@@ -0,0 +1,11 @@
+nums=[1,7,8,4,5,6,9,2]
+def selection_sort(nums):
+    n=len(nums)
+    for i in range(0,n):
+        min_idx=i
+        for j in range(i+1,n):
+            if(nums[j]<nums[min_idx]):
+                min_idx=j
+        nums[i],nums[min_idx]=nums[min_idx],nums[i]
+    return nums
+print(selection_sort(nums))
