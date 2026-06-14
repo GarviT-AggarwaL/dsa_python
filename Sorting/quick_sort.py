0316a9b80c58c8e664b0ffdebba9357a2eaa2b2c
unknown <aggarwalgarvit1304@gmail.com>
2026-06-14 13:51:01 +0530

    performing quick_sort using recursion technique

---
+++ Sorting/quick_sort.py
@@ -0,0 +1,32 @@
+nums = [4,1,7,6,3,2,8]
+
+def partition(nums, low, high):
+    pivot = nums[low]
+    i = low
+    j = high
+
+    while i < j:
+
+        while i <= high - 1 and nums[i] <= pivot:
+            i += 1
+
+        while j >= low + 1 and nums[j] > pivot:
+            j -= 1
+
+        if i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+
+    nums[low], nums[j] = nums[j], nums[low]
+    return j
+
+
+def quick_sort(nums, low, high):
+    if low < high:
+        p_idx = partition(nums, low, high)
+
+        quick_sort(nums, low, p_idx - 1)
+        quick_sort(nums, p_idx + 1, high)
+
+
+quick_sort(nums, 0, len(nums)-1)
+print(nums)
