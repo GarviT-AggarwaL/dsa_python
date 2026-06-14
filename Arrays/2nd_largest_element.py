bf5c2e486f730c72132f9f9fcbd3a058c2df56af
unknown <aggarwalgarvit1304@gmail.com>
2026-06-14 15:42:44 +0530

    finding 2nd largest element in array using brute,better and optimal approach

---
+++ Arrays/2nd_largest_element.py
@@ -0,0 +1,28 @@
+nums=[55,32,97,-55,45,32,88,21] 
+#brute
+nums.sort()
+print(nums[-2])
+
+#better
+largest=float("-inf")
+s_largest=float("-inf")
+n=len(nums)
+for i in range(0,n):
+    largest=max(largest,nums[i])
+for i in range(0,n):
+    if nums[i]>s_largest and nums[i]!=largest:
+        s_largest=nums[i]
+print(s_largest)
+    
+
+#optimal
+largest=float("-inf")
+s_largest=float("-inf")
+n=len(nums)
+for i in range(0,n):
+    if(nums[i]>largest):
+        s_largest=largest
+        largest=nums[i]
+    elif nums[i]>s_largest and nums[i]!=largest:
+        s_largest=nums[i]
+print(s_largest)
